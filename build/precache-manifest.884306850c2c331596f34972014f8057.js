a0a7b179b88fa189eaba370d5762f7036d1fdf8a
The-Kyle <oejava@gmail.com>
2019-03-05 18:53:20 -0800

    commit - http/https issue on github

--- build/precache-manifest.884306850c2c331596f34972014f8057.js
+++ build/precache-manifest.884306850c2c331596f34972014f8057.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tvmaze/static/js/runtime~main.5e95a82f.js"
   },
   {
-    "revision": "3a2d80853d63378b149c",
-    "url": "/tvmaze/static/js/main.3a2d8085.chunk.js"
+    "revision": "437659ce57adc4870f10",
+    "url": "/tvmaze/static/js/main.437659ce.chunk.js"
   },
   {
     "revision": "a6243badb86619d26395",
     "url": "/tvmaze/static/js/2.a6243bad.chunk.js"
   },
   {
-    "revision": "3a2d80853d63378b149c",
+    "revision": "437659ce57adc4870f10",
     "url": "/tvmaze/static/css/main.12b91101.chunk.css"
   },
   {
-    "revision": "c83def869568f3617c71cb7af33e9046",
+    "revision": "1ce3a60ae0e2d7637c423fdee471e075",
     "url": "/tvmaze/index.html"
   }
 ];
